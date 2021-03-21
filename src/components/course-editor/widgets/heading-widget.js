31bc98016dee25911fe8369f4f53aca1781acdb2
iking <king.id@husky.neu.edu>
2021-03-21 15:21:45 -0400

    changed paragraph wid to compare Strings

--- src/components/course-editor/widgets/heading-widget.js
+++ src/components/course-editor/widgets/heading-widget.js
@@ -14,12 +14,12 @@ const HeadingWidget = (
             {
                 editingWid !== widget.id &&
                 <div className="col">
-                    {widget.size === 1 && <h1>{widget.text}</h1>}
-                    {widget.size === 2 && <h2>{widget.text}</h2>}
-                    {widget.size === 3 && <h3>{widget.text}</h3>}
-                    {widget.size === 4 && <h4>{widget.text}</h4>}
-                    {widget.size === 5 && <h5>{widget.text}</h5>}
-                    {widget.size === 6 && <h6>{widget.text}</h6>}
+                    {widget.size.toString() === "1" && <h1>{widget.text}</h1>}
+                    {widget.size.toString() === "2" && <h2>{widget.text}</h2>}
+                    {widget.size.toString() === "3" && <h3>{widget.text}</h3>}
+                    {widget.size.toString() === "4" && <h4>{widget.text}</h4>}
+                    {widget.size.toString() === "5" && <h5>{widget.text}</h5>}
+                    {widget.size.toString() === "6" && <h6>{widget.text}</h6>}
                 </div>
             }
             <EditableWidget
